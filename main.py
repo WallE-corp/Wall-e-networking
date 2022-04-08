2b87fdb970019a5bb6458face74a87c3fe41a4fe
tifufu <dematasjoshua@hotmail.com>
2022-04-08 07:09:20 +0200

    Moved example code from command_handler.py to main.py outside of the package

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import wallE_networking as wen
+
 if __name__ == "__main__":
-  print("Nekopara!")
+  ch = wen.WallECommandHandler()
+
+
+  @ch.move('left')
+  def left(action):
+    print(action, 'moving left')
+
+
+  @ch.move('right')
+  def right(action):
+    print(action, 'moving right')
+
+
+  @ch.move('forward')
+  def forward(action):
+    print(action, 'moving forward')
+
+
+  @ch.move('backward')
+  def backward(action):
+    print(action, 'moving backward')
+
+
+  ch.start_listening()
+
--- wallE_networking/command_handler.py
+++ wallE_networking/command_handler.py
@@ -42,32 +42,3 @@ class WallECommandHandler(object):
       self.movement_commands[movement] = func
 
     return decorator_move
-
-
-"""
-ch = WallECommandHandler()
-
-
-@ch.move('left')
-def left(action):
-  print(action, 'moving left')
-
-
-@ch.move('right')
-def right(action):
-  print(action, 'moving right')
-
-
-@ch.move('forward')
-def forward(action):
-  print(action, 'moving forward')
-
-
-@ch.move('backward')
-def backward(action):
-  print(action, 'moving backward')
-
-
-ch.start_listening()
-
-"""
